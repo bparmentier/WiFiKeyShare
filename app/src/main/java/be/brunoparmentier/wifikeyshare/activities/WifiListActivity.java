fa7ff43cd7e74036f23b5124e0733a1e829d3244
Bruno Parmentier <dev@brunoparmentier.be>
2016-01-24 21:41:10 +0100

    Remove useless debug code

--- app/src/main/java/be/brunoparmentier/wifikeyshare/activities/WifiListActivity.java
+++ app/src/main/java/be/brunoparmentier/wifikeyshare/activities/WifiListActivity.java
@@ -328,7 +328,6 @@ public class WifiListActivity extends AppCompatActivity {
                 }
                 setSavedKeysToWifiNetworks();
             }
-            addWifiNetwork();
         }
     }
 
